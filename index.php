451f4673e42cb15f1bd14f6e3849a84e8dcd72d7
Dombus <bartosz.dabrowski97@gmail.com>
2019-03-18 20:53:51 +0100

    1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Projekt</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+      <div class="content">
+      </div>
+
+    </div>
+  </body>
+</html>
