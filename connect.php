6665b7a47886581d1ece8e25180463b526308dc0
BartoszDabrowski <48564005+BartoszDabrowski@users.noreply.github.com>
2019-06-24 12:45:23 +0200

    Update connect.php
    poprawki

--- connect.php
+++ connect.php
@@ -5,21 +5,24 @@
     $haslo = $_POST['password'];
     $login= htmlentities($login,ENT_QUOTES,UTF8);
     $haslo= md5($haslo,FALSE); //hash po md5
+    //$haslo = password_hash($haslo,PASSWORD_ARGON2ID); uzyj tego
     if ($connect->connect_errno!=0) {
       echo "error ".$connect->connect_error;
     }else {
-      $sql= "SELECT * FROM users where name='$login' and pass='$haslo'"; //zapytanie
-      //if($result = @$connect->query(sprintf("SELECT * FROM users where name='%s' and pass='%s'",
-      //mysqli_real_escape_string($connect,$login),mysqli_real_escape_string($connect,$login)))){
+      $sql= "SELECT * FROM users where name='$login' and pass='$haslo'"; //zapytanie bez porownywania hasla daj
       if ($result = @$connect->query($sql)) {
-        // code...
-
+        
         if ($result->num_rows) {
           $rekord = $result->fetch_assoc();
+          //$_SESSION['hash']= to jak sie to miejsce w bazie nazywa xD
+          //if(password_verify($haslo,$_SESSION['hash'])){  
           $_SESSION['logged'] = true;
           $_SESSION['user'] = $rekord['name'];
           $_SESSION['id'] = $rekord['id'];
-
+        //}else{
+        //    $_SESSION['error'] = "Nieprawidłowy login lub haslo";
+        //    header('Location:index.php');  
+        //}  jakos tak to bedzie
 
           // $rekord->close();
           header('Location: zalogowano.php');
